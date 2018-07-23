c14b0bc6c2555ccd17ca013701cf72f1e9ea6daf
Korver <korver2017@gmail.com>
2018-07-24 01:14:41 +0800

    change promise syntax to asyc/await

--- js/index.js
+++ js/index.js
@@ -3,48 +3,51 @@ let second = document.querySelector('.second');
 let timer = document.querySelector('.timer');
 let image = document.querySelector('img');
 
-function callApi() {
+function callApi(arg) {
   return new Promise((resolve, reject) => {
     if (true) {
-      resolve(fetch('https://randomuser.me/api/', {})
-        .then((response) => {
-          // console.log(response);
-          return response.json();
-        }).then((jsonData) => {
-          // console.log(jsonData);
-          // console.log(jsonData.results[0].email);
-          first.innerText = jsonData.results[0].email;
-        }).catch((err) => {
-          console.log('錯誤:', err);
-        })
-      )
+      resolve(
+        fetch('https://randomuser.me/api/', {})
+          .then(response => {
+            // console.log(response);
+            return response.json();
+          })
+          .then(jsonData => {
+            // console.log(jsonData);
+            console.log(jsonData.results[0].email);
+            arg.innerText = jsonData.results[0].email;
+          })
+          .catch(err => {
+            console.log('錯誤:', err);
+          })
+      );
     } else {
       reject('Reject!');
     }
-  })
+  });
 }
-callApi().then(setTimeout(() => {
-  timer.innerText = '2s Time Up!';
-  fetch('https://dog.ceo/api/breeds/image/random', {})
-    .then((response) => {
-      // console.log(response);
-      return response.json();
-    }).then((jsonData) => {
-      // console.log(jsonData);
-      // console.log(jsonData.message);
-      image.src = jsonData.message;
-    }).catch((err) => {
-      console.log('錯誤:', err);
-    });
-  fetch('https://randomuser.me/api/', {})
-    .then((response) => {
-      // console.log(response);
-      return response.json();
-    }).then((jsonData) => {
-      // console.log(jsonData);
-      // console.log(jsonData.results[0].email);
-      second.innerText = jsonData.results[0].email;
-    }).catch((err) => {
-      console.log('錯誤:', err);
-    })
-}, 2000));
+
+async function asyncAwait() {
+  callApi(first);
+  try {
+    setTimeout(() => {
+      fetch('https://dog.ceo/api/breeds/image/random', {})
+        .then(response => {
+          // console.log(response);
+          return response.json();
+        })
+        .then(jsonData => {
+          console.log(jsonData.message);
+          image.src = jsonData.message;
+        })
+        .catch(err => {
+          console.log('錯誤:', err);
+        });
+      timer.innerText = '2s Time Up!';
+      callApi(second);
+    }, 2000);
+  } catch (err) {
+    err;
+  }
+}
+asyncAwait();
