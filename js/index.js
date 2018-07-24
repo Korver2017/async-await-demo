6411660d398f6423a8ba62e4f198f5d678ea9377
Korver <korver2017@gmail.com>
2018-07-24 23:48:36 +0800

    add a new async/await instance to call randomuser

--- js/index.js
+++ js/index.js
@@ -3,27 +3,27 @@ let second = document.querySelector('.second');
 let timer = document.querySelector('.timer');
 let image = document.querySelector('img');
 
-async function callRandomUser() {
-  let data = 'https://randomuser.me/api/';
-  let xhr = new XMLHttpRequest();
-  xhr.open('get', data, true);
-  xhr.send(null);
-  xhr.onload = () => {
-    let json = JSON.parse(xhr.responseText);
-    console.log(json);
-    console.log(json.results[0].email);
-    return json.results[0].email;
-  };
+function callRandomUser() {
+  return new Promise((resolve, reject) => {
+    let data = 'https://randomuser.me/api/';
+    let xhr = new XMLHttpRequest();
+    xhr.open('get', data, true);
+    xhr.send(null);
+    xhr.onload = () => {
+      let json = JSON.parse(xhr.responseText);
+      console.log(json);
+      console.log(json.results[0].email);
+      resolve(json.results[0].email);
+    };
+  });
 }
 
-async function runFunc() {
-  let firstRandomUser = await callRandomUser();
-  console.log(firstRandomUser);
-  let secondRandomUser = await callRandomUser();
-  console.log(secondRandomUser);
-}
-
-runFunc();
+(async function runFunc() {
+  let firstUser = await callRandomUser();
+  let secondUser = await callRandomUser();
+  console.log(firstUser);
+  console.log(secondUser);
+})();
 
 // async function run() {
 //   return 'Success!';
